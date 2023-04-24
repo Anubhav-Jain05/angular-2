7cb70f07ce77f101da6f35761c7893e0d28a00a8
Anubhav Jain <anubhav.jain_cs19@gla.ac.in>
2023-04-24 18:45:50 +0530

    completeeeeeeee

--- src/app/services/cart/cart.component.ts
+++ src/app/services/cart/cart.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+constructor() {}
 
+ngOnInit():void {
+this.cartDet();
+this.loadcart();
+}
+getCartDet:any=[]; 
+cartDet(){
+  if(localStorage.getItem('localCart')){
+    // JSON.parse(localStorage.getItem('localCart')|| '[]' );
+    this.getCartDet.push(...JSON.parse(localStorage.getItem('localCart')|| '[]' ));
+    
+    console.log(this.getCartDet);
+  }
+}
+incQnt(prodId:any, qnt:any){
+  // console.log(prodId);
+  // console.log(qnt);
+  for(let i=0; i<this.getCartDet.length; i++){
+    if(this.getCartDet[i].prodId===prodId){
+      // qnt + 1;
+      if(qnt != 5)
+      this.getCartDet[i].qnt=parseInt(qnt) + 1;
+    }
+  }
+  localStorage.setItem('localCart',JSON.stringify(this.getCartDet));
+  this.loadcart();
+
+}
+decQnt(prodId:any, qnt:any){
+  // console.log(prodId);
+  // console.log(qnt);
+  for(let i=0; i<this.getCartDet.length; i++){
+    if(this.getCartDet[i].prodId===prodId){
+      // qnt + 1;
+      if(qnt != 1)
+      this.getCartDet[i].qnt=parseInt(qnt) - 1;
+    }
+  }
+  localStorage.setItem('localCart',JSON.stringify(this.getCartDet));
+  this.loadcart();
+
+}
+total:number=0;
+loadcart(){
+  if(localStorage.getItem('localCart')){
+    // this.getCartDet=JSON.parse(localStorage.getItem('localCart'));
+    // this.getCartDet.push({...JSON.parse(localStorage.getItem('localCart')|| '[]' )})
+    this.total=this.getCartDet.reduce(function(acc:any, val:any){
+      return acc + (val.amt * val.qnt);
+    }, 0)
+  }
+}
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+ 
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
+ constructor() { }
+ 
+ ngOnInit()  {
+   
+ }  
+ productArray = [
+  {
+    prodId:1,
+    img: "https://cdn.pixabay.com/photo/2020/03/09/06/18/camera-4914690__340.jpg",
+    amt: 400,
+    qnt: 1
+  },
+  {
+    prodId:2,
+    img: "https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762__340.jpg",
+    amt: 500,
+    qnt: 1
+  },
+  {
+    prodId:3,
+    img: "https://cdn.pixabay.com/photo/2021/01/22/16/55/camera-5940588__340.jpg",
+    amt: 600,
+    qnt: 1
+  }
+ ];
+ inc(prod:any) {
+  console.log(prod);
+  if(prod.qnt != 5){
+    prod.qnt += 1;
+  }
+ 
+ }
+ dec(prod:any){
+  console.log(prod);
+  if(prod.qnt !=1){
+    prod.qnt -= 1
+  } 
+ }
+ itemsCart:any=[];
+ addCart(category:any){
+  console.log(category);
+  let cartDataNull = localStorage.getItem('localCart');
+  if(cartDataNull == null){
+    let storeDataGet:any=[];
+    storeDataGet.push(category);
+    localStorage.setItem('localCart',JSON.stringify(storeDataGet));
+  }
+  else{
+    var id=category.prodId;
+    let index:number=-1;
+    // this.itemsCart=JSON.parse(localStorage.getItem('localCart'));
+    this.itemsCart.push(...JSON.parse(localStorage.getItem('localCart')|| '[]' ));
 
+
+    for(let i=0;i<this.itemsCart.length;i++){
+      if(parseInt(id)=== parseInt(this.itemsCart[i].prodId)){
+          this.itemsCart[i].qnt=category.qnt;
+          index=i;
+          break;
+      }
+    }
+    if(index==-1){
+      this.itemsCart.push(category);
+      localStorage.setItem('localCart',JSON.stringify(this.itemsCart));
+    }
+    else{
+      localStorage.setItem('localCart',JSON.stringify(this.itemsCart));
+    }
+  }
+  // localStorage.setItem('localCart',JSON.stringify(category));
+
+ }
 }
